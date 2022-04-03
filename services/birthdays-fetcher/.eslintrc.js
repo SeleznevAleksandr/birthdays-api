c68eaa17c5be2b0c6f9ed78089d4d18d4a0f12db
Aleksandr Seleznev <aleksandr.seleznev@flatstack.com>
2022-04-03 13:27:40 +0200

    Setup yarn & eslint & typescript config files

---
+++ services/birthdays-fetcher/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2016',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
